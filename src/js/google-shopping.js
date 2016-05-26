c66f88441bcae9c9effebf351e2ba7d37594c81f
Josh McBroom <mcbroomjc@gmail.com>
2016-05-25 17:41:53 -0700

    Turning in Google Shopping deliverable

--- src/js/google-shopping.js
+++ src/js/google-shopping.js
@@ -2,4 +2,55 @@ var data = require('../products.json');
 
 // Write your solutions below
 
-console.log(data.items);
+//================ Problem One ===================//
+
+// var itemCount = 0;
+
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].kind === "shopping#product") {
+//     // console.log(data.items[i]);
+//     itemCount += 1;
+//   }
+// }
+
+// console.log(itemCount);
+
+//================ Problem Two ==================//
+
+// for (var i = 0; i <data.items.length; i++) {
+//   if (data.items[i].product.inventories[0].availability == "backorder") {
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+//================ Problem Three =================//
+
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].product.images.length > 1) {
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+//================ Problem Four ==================//
+
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].product.brand == "Canon") {
+//     console.log(data.items[i].product.title);
+//   }
+// }
+
+//================ Problem Five ==================//
+
+// for (var i = 0; i < data.items.length; i++) {
+//   if (data.items[i].product.author.name == "eBay" && data.items[i].product.brand =="Canon") {
+//     console.log(data.items[i]);
+//   }
+// }
+
+//================ Problem Six ===================//
+
+// for (var i = 0; i < data.items.length; i++) {
+//   console.log(data.items[i].product.brand + " - $" + data.items[i].product.inventories[0].price)
+//   console.log(data.items[i].product.images[0].link)
+//   console.log("\n\n");
+// }
